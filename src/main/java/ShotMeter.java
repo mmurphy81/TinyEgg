c33e52e1f23e244dcb0de8a45447d43b56a81c0b
meramurphy <mera.murphy@menloschool.org>
2026-04-26 21:48:27 -0700

    worked on drawing the speed meter and setting how fast each one is

--- src/main/java/ShotMeter.java
+++ src/main/java/ShotMeter.java
@@ -1,12 +1,12 @@
 import java.awt.*;
 
 public class ShotMeter {
-    private double power;
     private double accuracy;
     private boolean isMoving;
     private int direction;
     private int barX;
     private int barY;
+    private int speed;
 
     public void update() {
 
@@ -21,11 +21,24 @@ public class ShotMeter {
         return 0;
     }
 
-    public void draw(Graphics g) {
-
+    public int getSpeed() {
+        return speed;
     }
 
-    public double getPowerMultiplier() {
-        return 0;
+    public void redRect(Graphics g) {
+        g.setColor(Color.red);
+        g.drawRect(100, 50, 100,100);
+        speed = 10;
     }
+    public void yellowRect(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawRect(180, 50, 80,100);
+        speed = 30;
+    }
+    public void greenRect(Graphics g){
+        g.setColor(Color.green);
+        g.drawRect(230, 50, 60,100);
+        speed = 50;
+    }
+
 }
