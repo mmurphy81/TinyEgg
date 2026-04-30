17f3fc414b38aa8a0ccbe8b317cc87e3e651414f
mmurphy81 <mera.murphy@menloschool.org>
2026-04-30 08:46:11 -0700

    Added the Shot meter  (#4)
    * fixed the shot meter
    
    * Fixed rebasing issues (NN)
    
    * Fixed comments to make code easier to understand

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -1,21 +1,56 @@
-public class GameEngine {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+// Implement actionlistener to create animatinos
+public class GameEngine implements ActionListener{
+    // Instance variables
     private GameDisplay window;
     private Egg activeEgg;
     private Level currentLevel;
     private int gameState;
     private double FRICTION;
     private int score;
+    private ShotMeter meter;
 
+    // Constants for state
+    public static final int STATE_OPENING = 0;
+    public static final int STATE_PLAYING = 1;
+
+    // Constructor that gives access to everything
     public GameEngine(){
-        window = new GameDisplay(this);
+        gameState = STATE_OPENING;
+        activeEgg = new Egg(600,345);
+        meter = new ShotMeter();
+        window = new GameDisplay(this, meter);
     }
 
-    public void update(){
+    // Updates the gamestate depending on egg
+    public void update() {
+        if (gameState == STATE_OPENING) {
+            activeEgg.updateOpening();
 
+            if (activeEgg.getState() == Egg.STATE_LANDED) {
+                gameState = STATE_PLAYING;
+            }
+        }
+        else if( gameState == STATE_PLAYING){
+            meter.update();
+        }
+
+        window.repaint();
+    }
+
+    public Egg getEgg() {
+        return activeEgg;
     }
 
     public void processShot(double deltaX, double deltaY, double multiplier){
 
+    }
+
+    public int getGameState() {
+        return gameState;
     }
 
     public void checkCollision(){
@@ -28,9 +63,20 @@ public class GameEngine {
     public String getFinalResult(){
         return null;
     }
+
+    // Timer for animation
     public void run() {
+        // 90 milliseconds of delay
+        Timer timer = new Timer(90, this);
+        timer.start();
 
     }
+
+    public void actionPerformed(ActionEvent e){
+
+        update();
+    }
+
     public static void main(String[] args) {
         GameEngine g = new GameEngine();
         g.run();
