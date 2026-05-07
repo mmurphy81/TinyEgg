e8c7a5250f8803de05d72a163a7afc8b2685276a
meramurphy <mera.murphy@menloschool.org>
2026-05-07 12:01:09 -0700

    add graspatch

---
+++ src/main/java/GrassPatch.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class GrassPatch extends Obstacle {
+    public GrassPatch(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public boolean hasCollided(Egg egg) {
+        return super.hasCollided(egg);
+    }
+
+    @Override
+    //Draws the grass
+    public void draw(Graphics g) {
+        g.setColor(GameDisplay.GRASS);
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
+    }
+
+}
